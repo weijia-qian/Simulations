08098a58ba16ca5d5833fde73920622e1179e863
Weijia Qian <ainibuni@gmail.com>
2025-10-23 22:05:07 -0400

    minor updates

--- Source/main_simulation_fttm_time.sh
+++ Source/main_simulation_fttm_time.sh
@@ -2,8 +2,8 @@
 #SBATCH --array=1-5%5
 #SBATCH --job-name=simulation_job
 #SBATCH --partition=wrobel
-#SBATCH --output=main_simulation_fttm.out
-#SBATCH --error=main_simulation_fttm.err
+#SBATCH --output=main_simulation_fttm_time.out
+#SBATCH --error=main_simulation_fttm_time.err
 
 module purge
 module load R/4.4.0
@@ -11,6 +11,6 @@ module load R/4.4.0
 # Rscript to run an r script
 # This stores which job is running (1, 2, 3, etc)
 JOBID=$SLURM_ARRAY_TASK_ID
-Rscript main_simulation_fttm.R $JOBID
+Rscript main_simulation_fttm_time.R $JOBID
 
 
